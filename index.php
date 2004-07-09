477eb813262c3a9b951913872136175230b3b942
miguel <arianne_rpg@users.sourceforge.net>
2004-07-09 18:39:29 +0000

    Fixed top menu and fixed a problem at news.xml

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="keywords" content="arianne, java, python, jython, multiplayer, play, online, game, engine, framework, content creation, C, portable, free, open source, gpl, MySQL, documentation, design">
     <meta name="description" content="Arianne is a multiplayer online games framework and engine to develop turn based and real time games. It provides a simple way of creating games on a portable and robust server architecture. The server is coded in Java and uses Python for your game description, provides a MySQL backend and uses an UDP transport channel to communicate with dozens of players.">
     <title>Arianne - A Multiplayer Online Role Playing Framework to develop games - </title>
-    <link rel="stylesheet" type="text/css" href="/cssdef.css">
+    <link rel="stylesheet" type="text/css" href="cssdef.css">
 </head>
 
 <body bgcolor="#D3CEEC">
