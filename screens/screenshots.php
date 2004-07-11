4fc447089cd776fd1f98fa62c4fbf17b3ed8511b
miguel <arianne_rpg@users.sourceforge.net>
2004-07-11 22:46:23 +0000

    no message

--- screens/screenshots.php
+++ screens/screenshots.php
@@ -66,11 +66,6 @@ else
   echo "<p><a href=\"index.php?arianne_url=screens/screenshots&archived=1\">Click here to see all the old images!</a>";
   }
   
-  echo '<p>';
-  foreach($_REQUEST as $i)
-    {
-    echo $i.'<br>';
-    }
 
 //******************************************************
 // This functions was named list_dir by whoever 
