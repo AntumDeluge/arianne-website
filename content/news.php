93e6d45a592eb58baad153b25ed0f0f5c64a7f91
miguel <arianne_rpg@users.sourceforge.net>
2005-04-12 06:50:45 +0000

    *** empty log message ***

--- content/news.php
+++ content/news.php
@@ -1,11 +1,4 @@
-
-      <h1>News items</h1>
-         If you wish to keep up to date with the latest changes, 
-         <a href="http://lists.sourceforge.net/mailman/listinfo/arianne-announce">click here</a>
-         or if you are looking for help please see the Get Help! section.
-         <p></p>
-      <h1>Latest news</h1>
-      <?php
+     <?php
         $all=isset($HTTP_GET_VARS['all']);
 
         $content=implode("",file('xml/website_news.xml'));
