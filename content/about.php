c58075d1ceb420351451f0d91ed5956c765e72f0
kymara <kymara@users.sourceforge.net>
2010-02-25 20:38:35 +0000

    tidy up this outdated descriptions for current project status

--- content/about.php
+++ content/about.php
@@ -1,21 +1,16 @@
 <div class="section"><h1>What is Arianne?</h1>
 
-<p>Arianne is a <i>multiplayer online games</i> framework and engine to develop 
-turn based and real time games.</p>
-
-<p>It provides a simple way of creating games on a <i>portable</i> and 
-<i>robust</i> server architecture. The server is coded in <i>Java</i> and uses
-<i>Python</i> for your game description, provides a <i>MySQL</i> backend and 
-uses an TCP transport channel to communicate with dozens of players.</p>
-
-<p>Our reference client engines are coded using <i>Java</i> and the <i>C</i> 
-language in order to achieve maximum portability.</p>
+<p>Arianne comprises a <i>multiplayer online games</i> framework and engine to develop 
+turn based and real time games, and the various games which use it.</p>
+The main subprojects active now are the framework/engine, called Marauroa, and a multiplayer online adventures game called Stendhal. 
+Some of the original subprojects are inactive now, you will find archived information about them on this site. 
 
 <p>Arianne has been in development since <i>1999</i> and has evolved from a 
 tiny application written in pseudo-C++ to a powerful, expandable but simple 
-server framework, running on the <i>Java</i> platform, and a portable client 
-framework, written in bare C to allow total portability of arianne's clients. 
-Arianne's server is totally <i>client agnostic</i>.</p>
+server framework called Marauroa, running on the <i>Java</i> platform. 
+Marauroa is totally <i>client agnostic</i>.</p>
+<p>Our reference client engines are coded using <i>Java</i> in order to achieve maximum portability.</p>
+
 
 <p>Since the beginning, the key concept at the heart of Arianne's development 
 has been <i>KISS</i>: Keep it simple, stupid! </p>
@@ -36,21 +31,21 @@ contributions and modifications to the code to create the best possible
 
 
 <h2>What is Marauroa?</h2>
-<p>All our efforts are supported by arianne's server: <i>Marauroa</i>.</p>
-<p>Marauroa is completely written in Java using a <i>multithreaded</i> server
-architecture with a <i>TCP</i> oriented network protocol, a <i>MySQL</i> based 
-persistence engine and a flexible game system. The game system is <i>totally 
-expandable and modifiable</i> by game developers and is able to run 
-<i>Python</i> scripts defining the game's rules.</p>
 
-<p>Marauroa is based on a design philosophy we called <i>Action - Perception</i>. 
+<p>All our efforts are supported by Arianne's engine: <i>Marauroa</i>, which is <i>portable</i> and
+<i>robust</i>.</p>
+<p>Marauroa is completely written in Java using a <i>multithreaded</i> server
+architecture with a <i>TCP</i> oriented network protocol, a <i>MySQL</i> or <i>H2</i> based 
+persistence engine and a flexible game system. The game system is <i>totally 
+expandable and modifiable</i> by game developers.</p>
+
+<p>Marauroa is based on an <i>Action - Perception</i> design philosophy. 
 Each turn a perception is send to the clients explaining what they currently 
 perceive. Clients can ask the server to perform any action in their name using 
 actions. Marauroa is totally game agnostic and makes very little assumptions about 
 what are you trying to do, allowing a great freedom to create any game type.</p>
 
-<p>Bugs are facts of life, they just happen. </p>
-<p>On Arianne we care about code quality so code is fully tested using Test Units with <i>JUnit</i> and <i>cppunit</i>, so all modules are tested for most common cases, allowing a better quality software to be deployed.
+<p>At Arianne we care about code quality so code is fully tested using Test Units with <i>JUnit</i>, so modules are tested for most common cases, allowing a better quality software to be deployed.
 </p><p><b>Your</b> cooperation reporting problems is invaluable: <i>You are our best developer and we want to hear from you</i>.</p>
 
 <p>If you'd like any more information, try our <a href="http://stendhal.game-host.org/wiki/index.php/Main_Page">wiki</a>.
