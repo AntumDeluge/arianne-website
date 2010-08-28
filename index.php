8d186af9fc1750cd1b20a86ffea6ce510dee2398
hendrik <nhnb@users.sourceforge.net>
2010-08-28 09:50:09 +0000

    fixed favicon link and use path relative to root for css

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
   <head>
-    <LINK REL="SHORTCUT ICON" HREF="http://arianne.game-server.cc/favicon.ico">
+    <LINK REL="SHORTCUT ICON" HREF="/favicon.ico">
     <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
     <meta name="keywords" content="arianne, java, python, jython, multiplayer, play, online, game, engine, framework, content creation, C, portable, free, open source, gpl, MySQL, documentation, design">
     <meta name="description" content="Arianne is a multiplayer online games framework and engine to develop turn based and real time games. It provides a simple way of creating games on a portable and robust server architecture. The server is coded in Java and uses Python for your game description, provides a MySQL backend and uses an UDP transport channel to communicate with dozens of players.">
-    <title>Arianne - A Multiplayer Online Role Playing Framework to develop games - </title>
+    <title>Arianne - A Multiplayer Online Role Playing Framework to develop games</title>
 <?php
 include_once('xml.php');
 
@@ -65,12 +65,12 @@ if( file_exists('xml/'.$name.'.xml')) {
 	$xml = XML_unserialize($content);
 
 	if( $xml['game'][0]['cssdoc']['0 attr']['url'] != "") {
- 		echo '<link rel="stylesheet" type="text/css" href="'.$xml['game'][0]['cssdoc']['0 attr']['url'].'">'; 
+ 		echo '<link rel="stylesheet" type="text/css" href="/'.$xml['game'][0]['cssdoc']['0 attr']['url'].'">'; 
 	} else {
-		echo '<link rel="stylesheet" type="text/css" href="css/cssdef.css">';
+		echo '<link rel="stylesheet" type="text/css" href="/css/cssdef.css">';
 	}
 } else {
-  echo '<link rel="stylesheet" type="text/css" href="css/cssdef.css">';
+  echo '<link rel="stylesheet" type="text/css" href="/css/cssdef.css">';
 }
 ?>
 
