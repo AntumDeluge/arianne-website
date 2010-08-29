087734fbc51952de222738c9e7c7138a61a974b7
hendrik <nhnb@users.sourceforge.net>
2010-08-29 17:06:25 +0000

    converted forgotten file to Page class

--- docs/tutorial/HowToWriteGamesUsingArianne.php
+++ docs/tutorial/HowToWriteGamesUsingArianne.php
@@ -1,1 +1,9 @@
-This webpage has been moved to <a href="http://arianne.sourceforge.net/wiki/index.php/HowToWriteGamesUsingArianne">http://arianne.sourceforge.net/wiki/index.php/HowToWriteGamesUsingArianne</a>
+<?php
+class Tutorial extends Page {
+
+	public function writeContent() {
+		echo 'This webpage has been moved to <a href="http://stendhalgame.org/wiki/HowToWriteGamesUsingArianne">'
+		. 'http://stendhalgame.org/wiki/HowToWriteGamesUsingArianne</a>.';
+	}
+}
+$page = new Tutorial();
