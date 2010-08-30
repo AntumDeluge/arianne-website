03cfee3e35804280717c5fbb0411ceebf87f2dc5
hendrik <nhnb@users.sourceforge.net>
2010-08-30 16:49:23 +0000

    use title of news item in title tag

--- content/news.php
+++ content/news.php
@@ -1,18 +1,32 @@
 <?php
 class News extends Page {
-	
-	public function writeContent() {
-		$all = isset($_REQUEST['all']) || (isset($_REQUEST['title']) && $_REQUEST['title'] == 'all');
-		$title = $_REQUEST['title'];
+	private $all;
+	private $title;
+	private $xml;
 
+	public function __construct() {
+		$this->all = isset($_REQUEST['all']) || (isset($_REQUEST['title']) && $_REQUEST['title'] == 'all');
+		$this->title = $_REQUEST['title'];
 		$content = implode("",file('xml/website_news.xml'));
-		$xml = XML_unserialize($content);
+		$this->xml = XML_unserialize($content);
+	}
 
-		if (!$all && isset($title)) {
-			WriteNewsHTMLForNamedItem($xml, $title);
+	public function writeHtmlHeader() {
+		if (!$this->all && isset($this->title)) {
+			WriteNewsHTMLHeaderForFirstItem($this->xml, $this->title);
+		} else {
+			echo '<title>A Multiplayer Online Role Playing Framework to develop games'.ARIANNE_TITLE.'</title>';
+			echo '<meta name="keywords" content="arianne, java, python, jython, multiplayer, play, online, game, engine, framework, content creation, C, portable, free, open source, gpl, MySQL, documentation, design">';
+			echo '<meta name="description" content="Arianne is a multiplayer online games framework and engine to develop turn based and real time games. It provides a simple way of creating games on a portable and robust server architecture. The server is coded in Java and uses Python for your game description, provides a MySQL backend and uses an UDP transport channel to communicate with dozens of players.">';
+		}
+	}
+
+	public function writeContent() {
+		if (!$this->all && isset($this->title)) {
+			WriteNewsHTMLForNamedItem($this->xml, $this->title);
 			echo '<p><a href="/">Read recent news items</a></p>';
 		} else {
-			WriteNewsHTML($xml, $all);
+			WriteNewsHTML($this->xml, $this->all);
 			echo '<p><a href="/news/all.html">Read all the news items</a></p>';
 		}
 	}
