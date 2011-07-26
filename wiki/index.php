50f4d579ce9510ec0e8d5a476f6984c0d4bc0081
hendrik <nhnb@users.sourceforge.net>
2011-07-26 20:40:23 +0000

    stripped of leading slash

--- wiki/index.php
+++ wiki/index.php
@@ -3,5 +3,5 @@
 if (isset($_REQUEST['title'])) {
 	header('Location: http://stendhalgame.org/wiki/'.urlencode($_REQUEST['title']));
 } else {
-	header('Location: http://stendhalgame.org/wiki/'.urlencode($_SERVER['PATH_INFO']));
+	header('Location: http://stendhalgame.org/wiki/'.urlencode(substr($_SERVER['PATH_INFO'], 1)));
 }
