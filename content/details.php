4084b208f590e68bd0360fd842ad27f69ff762a6
hendrik <nhnb@users.sourceforge.net>
2011-11-29 21:32:42 +0000

    handle not found in a nice way, including illegal page names

--- content/details.php
+++ content/details.php
@@ -1,18 +1,56 @@
-<?php 
+<?php
+/**
+ * renders the detail page of a subproject
+ *
+ * @author hendrik
+ */
 class DetailPage extends Page {
+	private $category;
+	private $xml;
 
-	public function writeContent() {
+	/**
+	 * checks the inputs and loads the xml definition
+	 */
+	public function __construct() {
+
+		// read and validate parameters
 		$name = $_REQUEST['name'];
 		if (!checkFilenameParameter($name)) {
-			// TODO: error message, return 404
 			return;
 		}
-		$temp = explode("/", $name);
-		
-		$content=implode("", file('xml/'.$temp[0].'_'.$temp[1].'.xml'));
-		$xml = XML_unserialize($content);
 
-		WriteGameHTML($xml, $temp[0]);
+		// create file name name and check existence
+		$temp = explode("/", $name);
+		$filename = 'xml/'.$temp[0].'_'.$temp[1].'.xml';
+		if (!file_exists($filename)) {
+			return;
+		}
+
+		// load xml file
+		$this->category = $temp[0];
+		$content=implode("", file($filename));
+		$this->xml = XML_unserialize($content);
+	}
+
+	/**
+	 * sends a 404 status code on non existing files
+	 */
+	public function writeHttpHeader() {
+		if (!isset($this->xml)) {
+			header('HTTP/1.1 404 Not found');
+		}
+		return true;
+	}
+
+	/**
+	 * writes the content of the sub project page
+	 */
+	public function writeContent() {
+		if (isset($this->xml)) {
+			WriteGameHTML($this->xml, $this->category);
+		} else {
+			echo 'Unfortunately this page does not exist.';
+		}
 	}
 }
 $page = new DetailPage();
