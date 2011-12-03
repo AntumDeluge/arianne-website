ff78b7a3ec1d2978ac886738143f2c53a0171484
hendrik <nhnb@users.sourceforge.net>
2011-12-03 17:10:40 +0000

    removed sf.net and w3c banners from footer

--- index.php
+++ index.php
@@ -163,20 +163,6 @@ if ($page->writeHttpHeader()) {
 			printf(' (Page loaded in %.3f seconds.)', $totaltime);
 			?>
 			</p>
-			<div class="footerimages">
-				<a href="http://sourceforge.net/projects/arianne">
-					<IMG SRC="http://sourceforge.net/sflogo.php?group_id=1111&amp;type=1"
-					ALT="SourceForge Logo" BORDER=0>
-				</a>
-				<a href="http://validator.w3.org/check?uri=referer">
-					<img style="border: 0; width: 88px; height: 31px"
-					src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01!">
-				</a>
-				<a href="http://jigsaw.w3.org/css-validator/">
-					<img style="border: 0; width: 88px; height: 31px"
-					src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!">
-				</a>
-			</div>
 		</div>
 	</div>
 </body>
