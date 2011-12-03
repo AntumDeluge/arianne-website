4860c0da6a1d1f3fd1b04e057d87e0fbcd9ab1d2
hendrik <nhnb@users.sourceforge.net>
2011-12-03 16:48:30 +0000

    removed leading empty lines

--- index.php
+++ index.php
@@ -69,11 +69,10 @@ include($page_url.".php");
 
 if ($page->writeHttpHeader()) {
 	header('Content-Type: text/html; charset=utf-8')
-?>
 
 
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <LINK REL="SHORTCUT ICON" HREF="/favicon.ico">
