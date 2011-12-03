1267e7004a1b81af011caf732457d47e8892fbcc
hendrik <nhnb@users.sourceforge.net>
2011-12-03 17:04:40 +0000

    cleaned up duplicated code

--- stendhal/greeting.php
+++ stendhal/greeting.php
@@ -6,15 +6,7 @@ if ($_REQUEST['product'] != 'stendhal') {
 	return;
 }
 
-
-
-
-if ($_REQUEST['from'] == 'null') {
-
-
-
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
 <head>
 <!-- <meta http-equiv="refresh" content="30; url=http://arianne.sf.net/"> --> 
@@ -45,73 +37,16 @@ img {
 </head>
 <body>
 <div style="margin-top: 200px; margin-left:100px">
-<h1 style="padding:0em">Downloading Stendhal... </h1>
-
-
-<p> Please be patient. A whole new world awaits...</p>
-
-</div>
-
-
-</body>
-</html>
-
-<?php 
-
-
-
-
-
-
-
+<?php
+if ($_REQUEST['from'] == 'null') {
+	echo '<h1 style="padding:0em">Downloading Stendhal... </h1>';
+	echo '<p> Please be patient. A whole new world awaits...</p>';
 } else {
-
-
-
-
-
-
+	echo '<h1 style="padding:0em">Updating Stendhal... </h1>';
+	echo '<p> Please be patient...</p>';
+}
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-<title>Stendhal</title>
-<style type="text/css">
-body {
-	font-family: Arial, Helvetica, "Liberation Sans", "Lucida Sans", Sans-serif;
-	font-size: 14pt;
-	width: 600px;
-	color: #000000;
-	background-repeat: no-repeat;
-	background-image: url("http://stendhalgame.org/images/header_background.jpg");
-	overflow:hidden;
-}
-img.news_image {
-	border: 1px solid black;
-	clear: right;
-	display: block;
-	float: right;
-	margin: 10px 0 10px 10px;
-	padding: 0;
-	position: relative;
-}
-img {
-	border: 1px solid black;
-}
-</style>
-</head>
-<body>
-<div style="margin-top: 200px; margin-left:100px">
-<h1 style="padding:0em">Updating Stendhal... </h1>
-
-
-<p> Please be patient...</p>
-
 </div>
-
 </body>
 </html>
-
-
-
-<?php }
+<?php
