43f9f92941cf7ef64ca6633cb857afcfd90dcc4d
hendrik <nhnb@users.sourceforge.net>
2011-12-11 18:13:47 +0000

    enabled CSP

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ require_once("renderContent.php");
 require_once("page.php");
 require_once("share_buttons.php");
 
+header('X-Frame-Options: sameorigin');
+header("X-Content-Security-Policy: default-src 'self'; img-src 'self' data: stendhalgame.org arianne.sf.net arianne.sourceforge.net sflogo.sourceforge.net sourceforge.net");
+
+
 define('ARIANNE_TITLE', ' &ndash; The Arianne Project');
 
 /**
