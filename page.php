5e3b548d2fb97c670b56c9960ca65388483c0bdb
hendrik <nhnb@users.sourceforge.net>
2011-12-15 06:42:34 +0000

    moved standard headers from page.php to news.php, so that they are only shown on the front page

--- page.php
+++ page.php
@@ -38,9 +38,6 @@ class Page {
 	 * this method can write additional html headers, for example the &lt;title&gt; tag.
 	 */
 	public function writeHtmlHeader() {
-		echo '<title>A Multiplayer Online Role Playing Framework to develop games'.ARIANNE_TITLE.'</title>';
-		echo '<meta name="keywords" content="arianne, java, python, jython, multiplayer, play, online, game, engine, framework, content creation, C, portable, free, open source, gpl, MySQL, documentation, design">';
-		echo '<meta name="description" content="Arianne is a multiplayer online games framework and engine to develop turn based and real time games. It provides a simple way of creating games on a portable and robust server architecture. The server is coded in Java and you may use Python for your game description, provides a MySQL or H2 database backend and uses an TCP transport channel to communicate with hundreds of players.">';
 	}
 
 	/**
